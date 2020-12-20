eaade8887bcbd0843cf3961d9c3cf5a6311d6857
fujinxiang <fujinxiang@163.com>
2020-12-20 12:09:43 +0800

    添加QrCode

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,10 +1,23 @@
-import React, { useEffect } from "react";
-import "./Popup.less";
+import React, { useEffect } from 'react';
+import './Popup.less';
 
 export default function Popup() {
   useEffect(() => {
     chrome.runtime.sendMessage({ popupMounted: true });
+
+    chrome.tabs.query({ active: true }, function (tabs) {
+      const url = tabs[0].url;
+      //@ts-ignore
+      QRCode.toCanvas(document.getElementById('qrcodeCanvas'), url, { width: 200, height: 200 }, function (error) {
+        if (error) console.error(error);
+        console.log('success!');
+      });
+    });
   }, []);
 
-  return <div className="popupContainer">Popup 页面</div>;
+  return (
+    <div className='popupContainer'>
+      <canvas id='qrcodeCanvas' />
+    </div>
+  );
 }
