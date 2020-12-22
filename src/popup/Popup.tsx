d6d781df6e4a885d817ad81241675673757110ed
付金祥 <fujinxiang@cvte.com>
2020-12-22 09:36:19 +0800

    添加复制URL和markdown

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -22,14 +22,35 @@ export default function Popup() {
     });
   };
 
+  const copyToClipboard = (text) => {
+    // @ts-ignore
+    const item = new ClipboardItem({ 'text/plain': new Blob([text], { type: 'text/plain' }) });
+    // @ts-ignore
+    navigator.clipboard.write([item]);
+  };
+
+  const copyUrl = () => {
+    chrome.tabs.query({ active: true }, function (tabs) {
+      const url = tabs[0].url;
+      copyToClipboard(url);
+    });
+  };
+
+  const copyMarkdown = () => {
+    chrome.tabs.query({ active: true }, function (tabs) {
+      const url = tabs[0].url;
+      const title = tabs[0].title;
+      copyToClipboard(`[${title}](${url})`);
+    });
+  };
+
   return (
     <div className='popupContainer'>
-      <button onClick={sendMessage}>测试发送消息到后台</button>
-      <button onClick={sendMessage}>url</button>
-      <button onClick={sendMessage}>markdown</button>
-      <div onClick={showQrcode} className='icon icon-markdown' />
+      <div onClick={copyUrl} className='icon icon-link' />
+      <div onClick={copyMarkdown} className='icon icon-markdown' />
       <div onClick={showQrcode} className='icon icon-qrcode' />
       <button onClick={sendMessage}>设置</button>
+      <button onClick={sendMessage}>测试发送消息到后台</button>
       <canvas id='qrcodeCanvas' />
     </div>
   );
