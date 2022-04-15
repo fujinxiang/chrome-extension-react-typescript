3eb58b08ca656587463d24dc70c878a0d0a72b27
付金祥 <fujinxiang@cvte.com>
2022-04-15 17:45:50 +0800

    添加代码打包分析

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production"
+  mode: "production",
+  plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'static' })],
 });
